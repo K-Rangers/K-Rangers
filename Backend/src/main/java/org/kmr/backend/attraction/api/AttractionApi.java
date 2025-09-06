57d732bf548358997d528cfeef666c1d7315d446
kiw0n <heeone2001@naver.com>
2025-09-06 14:14:33 +0900

    [refactor] api-endpoint-path

--- Backend/src/main/java/org/kmr/backend/attraction/api/AttractionApi.java
+++ Backend/src/main/java/org/kmr/backend/attraction/api/AttractionApi.java
@@ -10,20 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/attractions")
+@RequestMapping("/v1/main/user/attractions")
 @RequiredArgsConstructor
 public class AttractionApi {
-
     private final AttractionService attractionService;
 
     @GetMapping("/by-district/{district}")
     public ResponseEntity<List<AttractionResponse>> getAttractionsByDistrict(@PathVariable DaeguDistrict district) {
         List<Attraction> attractions = attractionService.findAttractionsByDistrict(district);
-
         List<AttractionResponse> responseDtos = attractions.stream()
                 .map(AttractionResponse::from)
                 .toList();
