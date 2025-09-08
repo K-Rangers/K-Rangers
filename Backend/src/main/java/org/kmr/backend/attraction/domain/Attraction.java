7044eb7f6508acba42de374a13c7cb24dbff1b71
eunseo16 <dmstj1116@naver.com>
2025-09-08 16:52:12 +0900

    feat: view

--- Backend/src/main/java/org/kmr/backend/attraction/domain/Attraction.java
+++ Backend/src/main/java/org/kmr/backend/attraction/domain/Attraction.java
@@ -1,12 +1,10 @@
 package org.kmr.backend.attraction.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.kmr.backend.common.TourCategory;
 
 @Entity
 @Getter
@@ -18,9 +16,35 @@ public class Attraction {
     private Long id;
     private String name;
     private String address;
+    private String elevator;
+    private String parking;
+    private String facility;
+    private String ramp;
+    private String informationCenter;
+    private String restroom;
+    private String wheelchairRental;
+    private String restaurant;
+    private String tableType;
+    private String lift;
 
-    public Attraction(String name, String address) {
+    @Enumerated(EnumType.STRING)
+    private TourCategory category;
+
+    public Attraction(String name, String address, String elevator, String parking, String facility,
+                      String ramp, String informationCenter, String restroom,
+                      String wheelchairRental, String restaurant, String tableType, String lift, TourCategory category) {
         this.name = name;
         this.address = address;
+        this.elevator = elevator;
+        this.parking = parking;
+        this.facility = facility;
+        this.ramp = ramp;
+        this.informationCenter = informationCenter;
+        this.restroom = restroom;
+        this.wheelchairRental = wheelchairRental;
+        this.restaurant = restaurant;
+        this.tableType = tableType;
+        this.lift = lift;
+        this.category = category;
     }
 }
