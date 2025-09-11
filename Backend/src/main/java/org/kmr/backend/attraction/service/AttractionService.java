696f3e2dec01e736a6fee8010c1c52d17e645ed5
eunseo16 <dmstj1116@naver.com>
2025-09-11 21:38:35 +0900

    [feat] address

--- Backend/src/main/java/org/kmr/backend/attraction/service/AttractionService.java
+++ Backend/src/main/java/org/kmr/backend/attraction/service/AttractionService.java
@@ -4,18 +4,30 @@ import lombok.RequiredArgsConstructor;
 import org.kmr.backend.attraction.domain.Attraction;
 import org.kmr.backend.attraction.repository.AttractionRepository;
 import org.kmr.backend.common.DaeguDistrict;
+import org.kmr.backend.review.ReviewService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AttractionService {
+
     private final AttractionRepository attractionRepository;
+    private final ReviewService reviewService;
 
     public List<Attraction> findAttractionsByDistrict(DaeguDistrict district) {
         String districtName = district.getKoreanName();
-        return attractionRepository.findByAddressContaining(districtName);
+        List<Attraction> attractions = attractionRepository.findByAddressContaining(districtName);
+
+        attractions.sort(Comparator.comparing(
+                attraction -> reviewService.getAvgRatingByAttraction(attraction.getId()),
+                Comparator.reverseOrder()
+        ));
+
+        return attractions;
     }
 }
